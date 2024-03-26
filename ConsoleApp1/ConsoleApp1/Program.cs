90f11d4dc721d4197a404b0f276b3e31a7e2256a
John Akeister <N1260026@my.ntu.ac.uk>
2024-03-26 13:47:22 +0000

    Visualization of how inputting tasks would work

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string[] tasks;
+            Console.WriteLine("Please enter a Project name");
+            string projectname = Console.ReadLine();
+            Console.WriteLine("How many team members will be assigned?");
+            int numofusers = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many tasks will be part of this project?");
+            int numoftasks = Convert.ToInt32(Console.ReadLine());
+            tasks = new string[numoftasks];
+            for (int i = 0; i < numoftasks; i++)
+            {
+                
+                Console.WriteLine("Please enter the task name");
+                tasks[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Project: "+ projectname + "\nList of tasks: ");
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                Console.WriteLine((i + 1) +") " + tasks[i]);
+
+            }
+            Console.ReadLine();
         }
     }
 }
